d81b3229618be0ec9ccc1f3001579aa53c01d498
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-09-17 16:39:50 +0400

    Pages will be added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Icon
+import "remixicon/fonts/remixicon.css";
+
+// Slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
