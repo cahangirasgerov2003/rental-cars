2ab790f3a0a82e3c26836b5c62f2c88998454f19
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-10-07 23:29:06 +0400

    Find car form will be added

---
+++ src/components/UI/FindYourCar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "../../styles/findYourCar.css";
+
+const FindYourCar = () => {
+  const formControl = () => {
+    const forms = document.querySelector(".needs-validation");
+    forms.classList.add("was-validated");
+  };
+
+  return (
+    <div className="container">
+      <form className="row needs-validation find__car__form" noValidate>
+        <div className="col-12">
+          <span className="d-flex align-items-center">
+            <p className="mb-0">Find your best car here ...</p>
+            <i className="ri-ball-pen-fill pen__ball"></i>
+          </span>
+        </div>
+        <div className="col-4">
+          <input
+            className="form-control"
+            type="text"
+            id="from__address"
+            placeholder="From address"
+            required
+          />
+          <div className="invalid-feedback">Please select a valid state.</div>
+        </div>
+
+        <div className="col-4">
+          <input
+            className="form-control"
+            type="text"
+            id="to__address"
+            placeholder="To address"
+            required
+          />
+          <div className="invalid-feedback">Please select a valid state.</div>
+        </div>
+        <div className="col-4">
+          <input
+            className="form-control"
+            type="date"
+            id="journey__date"
+            required
+          />
+          <div className="invalid-feedback">Please select a valid state.</div>
+        </div>
+        <div className="col-4">
+          <input
+            className="form-control"
+            type="time"
+            id="journey__time"
+            defaultValue="0"
+            required
+          />
+          <div className="invalid-feedback">Please select a valid state.</div>
+        </div>
+
+        <div className="col-4 selectFindCar">
+          <select
+            className="form-select"
+            name="AC__type"
+            id="AC__type"
+            defaultValue="AC Car"
+            onChange={() => {
+              console.log("a");
+            }}
+          >
+            <option value="AC Car">AC Car</option>
+            <option value="Non AC Car">Non AC Car</option>
+          </select>
+        </div>
+
+        <div className="col-4">
+          <button
+            type="button"
+            className="button w-100"
+            onClick={() => {
+              formControl();
+            }}
+          >
+            Find car
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FindYourCar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "../components/Helmet/Helmet";
 import ReserveSlider from "../components/UI/ReserveSlider";
+import FindYourCar from "../components/UI/FindYourCar";
 
 const Home = () => {
   return (
@@ -8,6 +9,17 @@ const Home = () => {
       <Helmet title="Home">
         <section className="reserve__slider__section">
           <ReserveSlider />
+
+          <div className="find__car__content w-100">
+            <div className="container">
+              <div className="row find__car__row">
+                <div className="col-4 find__car__left"></div>
+                <div className="col-8 find__car__right">
+                  <FindYourCar />
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </Helmet>
     </React.Fragment>
